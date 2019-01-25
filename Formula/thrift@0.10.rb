39c71e70ab3bac68356f26b7a08ce5391f8ca18b
Suhas Gaddam <sgaddam@axon.com>
2019-01-25 12:15:33 -0800

    Remove python dependence

--- Formula/thrift@0.10.rb
+++ Formula/thrift@0.10.rb
@@ -32,7 +32,7 @@ class ThriftAT010 < Formula
   depends_on "boost"
   depends_on "openssl"
   depends_on "libevent" => :optional
-  depends_on :python => :optional
+  # depends_on :python => :optional
 
   def install
     system "./bootstrap.sh" unless build.stable?
